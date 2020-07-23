3512eacd90c6acc2c1ffebe4caa8287098e8d3ab
yuchaehwi <nancy10222naver.com>
2020-07-23 14:53:09 +0900

    setTimeOut

--- src/App.js
+++ src/App.js
@@ -21,6 +21,54 @@ class Lotto extends PureComponent {
       redo : false
    };
    
+
+   timeouts = [];
+
+   runTimeouts = () => {
+      const { winNumbers } = this.state;
+      for (let i = 0; i < winNumbers.length - 1; i++) {
+         this.timeouts[i] = setTimeout(() => {
+            this.setState((preState) => {
+               return {
+                  winBalls: [...preState.winBalls, winNumbers[i]]
+               }
+            })
+         },(i+1)*1000);
+      }
+      this.timeouts[6] = setTimeout(()=>{
+         this.setState({
+               bonus : winNumbers[6],
+               redo : true
+         });
+      },7000)
+   }
+
+   componentDidMount = () => {
+      this.runTimeouts();
+   }
+
+   componentDidUpdate = (prevProps, prevState) => {
+      if(this.state.winBalls.length === 0){
+         this.runTimeouts();
+      }
+   }
+
+   componentWillUnmount = () => {
+      this.timeouts.forEach((v)=>{
+         clearTimeout(v);
+      });
+   }
+
+   onClickRedo = () => {
+      this.setState({
+         winNumbers: getWinnumbers(),
+         winBalls: [],
+         bonus: null,
+         redo: false
+      });
+      this.timeouts = [];
+   }
+
    render() {
       return (
          <div></div>
