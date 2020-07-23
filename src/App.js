9327fd863acfde39642e5ff5a9d0246bdc22b2fb
yuchaehwi <nancy10222naver.com>
2020-07-23 11:48:47 +0900

    getWinNumbers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 
+function getWinnumbers(){
+   const candidate = Array(45).fill().map((v,i)=>i+1);
+   const shuffle = [];
+   while(candidate.length>0){
+      shuffle.push(candidate.splice(Math.floor(Math.random()*candidate.length),1)[0]);
+   }
+   const bonusNumber = shuffle[shuffle.length-1];
+   const winNumbers = shuffle.slice(0,6).sort((p,c)=>p-c);
+   return [...winNumbers,bonusNumber];
+}
+
 class Lotto extends PureComponent {
    render() {
       return (
